fa275aeb86058843756809894313fca90ae62503
Jatin <jy799914@gmail.com>
2026-04-19 17:50:32 +0530

    updated file

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,6 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
-      // base: "/Portfolio",
+       base: "/Portfolio",
   },
 ])
